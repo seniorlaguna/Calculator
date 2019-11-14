ef3ea37b1b52e9d476968e85c0754a5df990f6ab
Your Name <you@example.com>
2019-11-14 11:47:19 +0100

    Updated Instructor library and added exception handling to url opening

--- app/src/main/java/org/seniorlaguna/calculator/utils/Branding.kt
+++ app/src/main/java/org/seniorlaguna/calculator/utils/Branding.kt
@@ -1,22 +1,21 @@
 package org.seniorlaguna.calculator.utils
 
-import android.content.Intent
 import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.LayoutInflaterCompat
 import androidx.preference.PreferenceManager
 import org.seniorlaguna.calculator.R
 
-private val APP_STARTS_BEFORE_ASKING_KEY = "APP_STARTS_BEFORE_ASKING_KEY"
-private val APP_STARTS_BEFORE_ASKING = 5
+private const val APP_STARTS_BEFORE_ASKING_KEY = "APP_STARTS_BEFORE_ASKING_KEY"
+private const val APP_STARTS_BEFORE_ASKING = 5
 
-private val playstore_market_package_url = "market://details?id=org.seniorlaguna.calculator"
-private val playstore_http_package_url = "https://play.google.com/store/apps/details?id=org.seniorlaguna.calculator"
-private val playstore_market_developer_url = "market://play.google.com/store/apps/developer?id=Senior+Laguna"
-private val playstore_http_developer_url = "https://play.google.com/store/apps/developer?id=Senior+Laguna"
-private val github_url = "https://github.com/seniorlaguna?tab=repositories"
+private const val playstore_market_package_url = "market://details?id=org.seniorlaguna.calculator"
+private const val playstore_http_package_url = "https://play.google.com/store/apps/details?id=org.seniorlaguna.calculator"
+private const val playstore_market_developer_url = "market://play.google.com/store/apps/developer?id=Senior+Laguna"
+private const val playstore_http_developer_url = "https://play.google.com/store/apps/developer?id=Senior+Laguna"
+private const val github_url = "https://github.com/seniorlaguna?tab=repositories"
 
 fun openPlaystore(context: Context, developer : Boolean = true) {
     try {
@@ -24,16 +23,22 @@ fun openPlaystore(context: Context, developer : Boolean = true) {
         intent.data = Uri.parse(if (developer) playstore_market_developer_url else playstore_market_package_url)
         context.startActivity(intent)
     } catch (e: ActivityNotFoundException) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse(if (developer) playstore_http_developer_url else playstore_http_package_url)
-        context.startActivity(intent)
+        openUrl(context, if (developer) playstore_http_developer_url else playstore_http_package_url)
     }
 }
 
 fun openGithub(context: Context) {
-    val intent = Intent(Intent.ACTION_VIEW)
-    intent.data = Uri.parse(github_url)
-    context.startActivity(intent)
+    openUrl(context, github_url)
+}
+
+private fun openUrl(context: Context, url : String) {
+    try {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        context.startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        // TODO("Add exception handling if no activity found")
+    }
 }
 
 fun askForAppRating(context: Context) {
@@ -45,17 +50,17 @@ fun askForAppRating(context: Context) {
     if (starts < 0) return
 
     // don't ask yet wait some time
-    if (starts < 5) {
-        prefs.edit().putInt(APP_STARTS_BEFORE_ASKING_KEY, starts + 1).commit()
+    if (starts < APP_STARTS_BEFORE_ASKING) {
+        prefs.edit().putInt(APP_STARTS_BEFORE_ASKING_KEY, starts + 1).apply()
     }
     else {
-        prefs.edit().putInt(APP_STARTS_BEFORE_ASKING_KEY, -1).commit()
+        prefs.edit().putInt(APP_STARTS_BEFORE_ASKING_KEY, -1).apply()
 
         // show request
         val builder = AlertDialog.Builder(context)
         builder.setView(R.layout.app_rating)
         builder.setNegativeButton(R.string.app_rating_request_no, null)
-        builder.setPositiveButton(R.string.app_rating_request_yes, {_, _ -> openPlaystore(context, false)})
+        builder.setPositiveButton(R.string.app_rating_request_yes) { _, _ -> openPlaystore(context, false)}
         builder.show()
     }
 
